61e26f035f7af252d4ea6d4bcf5c8cb658fbe205
MonikaJhingan <jhinganmonika750@gmail.com>
2025-06-15 23:32:19 +0530

    New Changes modified readme

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # Delivery-Label-Generator
+
+A Minimal web-based dashboard to genrate Delivery labels.
+[🔗 Live Demo](https://delivery-label-generator.onrender.com)
+
+## Setup Instructions
+
+### Requirements
+- Node.js(v14+)
+
+### 1.Clone the repo
+```bash
+git clone https://github.com/MonikaJhingan/Delivery-Label-Generator.git
+cd Delivery-Label-Generator
+```
+
+### 2.Install Dependencies
+npm install
+
+### 3.Run the Server
+node server.js
+
+-The app wil start on 
+
+```http://localhost:5000```
+
+### 4. Login Credentials
+-Username:test
+-password:test123
+
+## Working
+- Vist login page at `/`
+- Submit valid credentials
+- Redirected to Dashboard
+- Fill sender Receiver Form
+- On Clicking Confirm, a PDF label is generated with
+   - Sender and Receiver Details
+   - Unique Delivery Id
+   - Barcode
+- Pdf gets downloaded automatically.
+
+## Tech stack
+- Node.js
+- Express
+- pdfkit (pdf generation)
+- bwip (barcode)
+- express-session
+- body-parser
+
+## Deployment Tips
+Deployed on Render with production build environment Live link given on top.
--- server.js
+++ server.js
@@ -1,98 +1,104 @@
-const express=require('express');
-const bodyParser=require('body-parser');
-const fs=require('fs');
-const session=require('express-session');
-const PDFKit=require('pdfkit');
-const bwipjs=require('bwip-js');
-const path=require('path');
-const { constants } = require('zlib');
+const express = require('express'); //Imported Express Server
+const bodyParser = require('body-parser'); // Parses form Data
+const fs = require('fs'); //File system Module
+const session = require('express-session'); //Session Management Handling
+const PDFKit = require('pdfkit'); // PDF Creation
+const bwipjs = require('bwip-js'); //Barcode Generator
+const path = require('path'); //Utility for  file paths
 
-const app=express();
-const PORT=process.env.PORT || 5000;
+// App Configurat\ion
+const app = express();
+const PORT = process.env.PORT || 5000; // Server PORT Number
 
-app.use(bodyParser.urlencoded({extended:true}));
+// ----------------Middleware Setup----------------------
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(session({
-   secret:'secret-key',
-   resave:false,
-   saveUninitialized:true,
-}));
+app.use(
+  session({
+    secret: 'secret-key', //Secret for signing session id cookies
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
+//------------User Credentials HardCoded------------------
+const Username = 'test';
+const Password = 'test123';
 
-//User authentication HardCoded
-const Username='test';
-const Password="test123";
+//---------------------Routes-----------------//
 
-//Login Page
-app.get('/',(req,res)=>{
-   res.sendFile(__dirname+'/views/login.html')
-})
+//Login Page(GET)
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/login.html');
+});
 
-//Handle User authentication
-app.post('/login',(req,res)=>{
-   const {username,password}=req.body;
-   if(username===Username && password===Password){
-   req.session.loggedIn=true;
-   res.redirect('/dashboard')
-   }else{
-      res.send('Invalid Login <a href='/'>Login Again</a>');
-   }
-   } )
+//Handle User authentication (POST)
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  if (username === Username && password === Password) {
+    req.session.loggedIn = true;
+    res.redirect('/dashboard');
+  } else {
+    res.send('Invalid Login <a href=' / '>Login Again</a>');
+  }
+});
 
-   //Dashboard Page
-   
-app.get('/dashboard',(req,res)=>{
-   if(req.session.loggedIn){
-   res.sendFile(__dirname+'/views/dashboard.html')
-   } else{
-      res.redirect('/');
-   }
-})
+//Dashboard Page(GET)
+app.get('/dashboard', (req, res) => {
+  if (req.session.loggedIn) {
+    res.sendFile(__dirname + '/views/dashboard.html');
+  } else {
+    res.redirect('/');
+  }
+});
 
-// PDF Generate
-app.post('/generate',async(req,res)=>{
- const {senderName,senderAddress,receiverName,receiverAddress}=req.body;
- const deliveryID='DLV-'+ Date.now();  //Unique Id
+// PDF Generate/Label Generate/Barcode Generate
+app.post('/generate', async (req, res) => {
+  const { senderName, senderAddress, receiverName, receiverAddress } = req.body;
+  const deliveryID = 'DLV-' + Date.now(); //Unique Delivery Id Genrated
 
- const doc=new PDFKit();
- const outputDir=path.join(__dirname,'generated');
- if(!fs.existsSync(outputDir)){
-   fs.mkdirSync(outputDir);
- }
- const filePath=path.join(outputDir,`${deliveryID}.pdf`);
- const stream=fs.createWriteStream(filePath);
- doc.pipe(stream);
+  const doc = new PDFKit();
+  //Check if generated folder exists
+  const outputDir = path.join(__dirname,'generated');
+  if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir);
+  }
+  //Create File path for new PDF
+  const filePath = path.join(outputDir, `${deliveryID}.pdf`);
+  const stream = fs.createWriteStream(filePath);
+  doc.pipe(stream);
 
-
- doc.fontSize(14).text(`Delivery ID:${deliveryID}`);
- doc.moveDown();
- doc.text(`Sender:${senderName}`);
- doc.moveDown();
- doc.text(senderAddress);
- doc.moveDown();
- doc.text(`Receiver:${receiverName}`); 
- doc.text(receiverAddress);
- doc.moveDown();
-
- try{
-   const png=await bwipjs.toBuffer({
-      bcid:'code128',
-      text:deliveryID,
-      scale:3,
-      height:10,
-      includetext:true
-   })
-   doc.image(png,{width:200});
- }catch(err){
-   doc.text('Error generating Barcode')
- }
-doc.end();
-
-stream.on('finish',()=>{
-   res.download(filePath);
-})
-
-})
-app.listen(PORT,()=>{
-   console.log(`Server running on PORT ${PORT}`)
-})
+  //Added Delivery Info to PDF
+  doc.fontSize(14).text(`Delivery ID:${deliveryID}`);
+  doc.moveDown();
+  doc.text(`Sender:${senderName}`);
+  doc.moveDown();
+  doc.text(senderAddress);
+  doc.moveDown();
+  doc.text(`Receiver:${receiverName}`);
+  doc.moveDown();
+  doc.text(receiverAddress);
+  doc.moveDown();
+  //Generated bar Code for Delivery Id
+  try {
+    const png = await bwipjs.toBuffer({
+      bcid: 'code128',
+      text: deliveryID,
+      scale: 3,
+      height: 10,
+      includetext: true,
+    });
+    doc.image(png, { width: 200 });
+  } catch (err) {
+    doc.text('Error generating Barcode');
+  }
+  doc.end();
+  //Send File to User after its finished
+  stream.on('finish', () => {
+    res.download(filePath);
+  });
+});
+//Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
