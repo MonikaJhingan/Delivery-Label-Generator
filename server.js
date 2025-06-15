b5ac60b642682420c18f73c134fff5f1734c40ec
MonikaJhingan <jhinganmonika750@gmail.com>
2025-06-15 20:31:02 +0530

    Modified port for deployment

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path=require('path');
 const { constants } = require('zlib');
 
 const app=express();
-const PORT=5000;
+const PORT=process.env.PORT||5000;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
